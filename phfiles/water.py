612740b88f3494f0d630a2926f1e5eca4c093124
Your Name <you@example.com>
2017-01-29 15:29:39 +0900

    change to the expected form

--- phfiles/water.py
+++ phfiles/water.py
@@ -10,14 +10,11 @@ def min(x,y):
    else:
       return y
 
-import sys
 
-argv = sys.argv
+number = int(raw_input("Enter the number of poles:"))
+n = raw_input("Enter the hight of poles:").split()
 
-number = int(argv[1])
-
-if number != (len(argv) - 2):
-   print"Usage: n,h_1,...h_n"
+if number != (len(n)):
    print"error number of pole(n) and the number you inputted is not same"
    quit()
 
@@ -25,7 +22,7 @@ out = 0
 
 for num in range(0,number-1):
     for num2 in range(num,number):
-        volume = min(int(argv[num2+2]),int(argv[num+2]))*(num2-num)
+        volume = min(int(n[num2]),int(n[num]))*(num2-num)
         out = max(out,volume)
 
 print "maximam volume = " + str(out)
