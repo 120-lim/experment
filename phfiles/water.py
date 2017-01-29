47c316e6e3da57ed1c0a23b607b37c80a8be1721
Your Name <you@example.com>
2017-01-29 10:05:50 +0900

    add sth

--- phfiles/water.py
+++ phfiles/water.py
@@ -1,9 +1,16 @@
 def max(x,y):
    if x > y:
       return x
-   else
+   else:
       return y
 
-water_tuple = (1,5,2,1,2,4,6)
+number = 7
+water = (1,5,2,1,2,4,6)
 
+out = 0
+for num in range(0,number-1):
+    for num2 in range(num,number):
+        volume = (water[num]-water[num2])*(num2-num)
+        out = max(out,volume)
 
+print "maximam =" + str(out)
