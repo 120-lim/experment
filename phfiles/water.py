57e82fcff1aa8553edcde5d6fb52f2a344d9b8f0
Your Name <you@example.com>
2017-01-29 10:09:48 +0900

    add sth

--- phfiles/water.py
+++ phfiles/water.py
@@ -4,13 +4,19 @@ def max(x,y):
    else:
       return y
 
+def min(x,y):
+   if x < y:
+      return x
+   else:
+      return y
+
 number = 7
 water = (1,5,2,1,2,4,6)
 
 out = 0
 for num in range(0,number-1):
     for num2 in range(num,number):
-        volume = (water[num]-water[num2])*(num2-num)
+        volume = min(water[num2],water[num])*(num2-num)
         out = max(out,volume)
 
 print "maximam =" + str(out)
