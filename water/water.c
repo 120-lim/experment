7c4f934722912290b8e1c4d530c556738c0eef8c
Your Name <you@example.com>
2017-01-31 22:36:21 +0900

    complete the c-code

--- water/water.c
+++ water/water.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 int max(int x,int y){
    if(x>y){
@@ -19,17 +21,39 @@ int min(int x,int y){
 }  
 
 int main(){
+/*take data from command line*/
    int number;
-   int *a;
-   printf("Enter the number of poles \n");
-   scanf("%d",&number);
-   printf("Enter the hight of poles \n");
-   scanf("%d", a);
+   char a[20000];
+   char *tp;
+   printf("Enter the number of poles: ");
+   fgets(a, sizeof(a), stdin);
+   number = atoi(a);
+   int b[number];
+   int num;
+   printf("Enter the hight of poles: ");
+   fgets(a,sizeof(a),stdin);
+   tp = strtok(a," ");
+   num = 1;
+   b[0] = atoi(tp);
+   while(tp != NULL){
+      tp = strtok(NULL," ");
+      if(tp != NULL){
+         b[num] = atoi(tp);
+         num += 1;
+      }
+   }
+
+   if(num != number){
+      printf("error:number of poles and the number you entered is not same \n");
+      exit(1);
+   }
+
+/*main calculation*/
    int volume = 0;
    int i,j;
    for(i=0;i<number-1;++i){
       for(j=i+1;j<number;++j){
-         volume = max((min(a[i],a[j])*(j-i)),volume);
+         volume = max((min(b[i],b[j])*(j-i)),volume);
       }
    }
    printf("maximam volume is %d \n",volume);
