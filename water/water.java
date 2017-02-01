264d3d7da09cbba327520c9b568ca5a2c58dd1b5
Your Name <you@example.com>
2017-02-01 14:59:54 +0900

    ll

--- water/water.java
+++ water/water.java
@@ -17,6 +17,15 @@ public class water {
 	public static void main(String[] args){
 		System.out.println("unko");
 		int a = 100;
+		int i,j;
 		System.out.println(a);
+
+		//ここからメインの計算
+		for(i=0;i<numberi-1;++i){
+			for(j=i+1;j<number;++j){
+				volume = max((min(b[i],b[j])*(j-i)),volume);
+			}
+		}
+		System.out.println("maximam is" volume);
 	}
 }
