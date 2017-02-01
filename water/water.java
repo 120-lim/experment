596a7ab0e0d64b7abd2ad7173fa59ddc3380045d
Your Name <you@example.com>
2017-02-01 12:00:08 +0900

    add java file

---
+++ water/water.java
@@ -0,0 +1,22 @@
+static int max(int a,int,b){
+	if(a>b){
+		return a;
+	}else{
+		return b;
+	}
+}
+static int min(int a,int b){
+	if(a<b){
+		return a;
+	}else{
+		return b;
+	}
+}
+
+public class unko {
+	public static void main(String[] args){
+		System.out.println("unko");
+		int a = 100;
+		System.out.println(a);
+	}
+}
